023c0d7d7e62b2141509fb48c090e1174eb5c52a
👨‍💻𝙎𝙖𝙝𝙖𝙣 𝙈𝙖𝙙𝙪𝙬𝙖𝙣𝙩𝙝𝙖 <125999503+MrMasterOfc@users.noreply.github.com>
2025-01-11 18:07:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your subzero md session id here",
+SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~M1JDFS4R#rAmggKTeOQdkmx82PbXoo6Rc_eotKXesXeY5ywLC1lI",
  
 PREFIX: process.env.PREFIX || ".",
 // Enter Your Desired Prefix
@@ -39,7 +39,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "263719647303",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "9472797915",
 // Set Owner Name
 
 OWNER_NAME: process.env.OWNER_NAME || "ᴍʀ ꜰʀᴀɴᴋ ᴏꜰᴄ",
